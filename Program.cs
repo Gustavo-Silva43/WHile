40af4963df2957c151834f1e44ab1981036edc30
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2022-10-28 15:38:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_while_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
